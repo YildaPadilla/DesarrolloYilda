9ceda5846d8f101d6f5e7855c8fcd563fd0c2238
YilPadi <98482944+YilPadi@users.noreply.github.com>
2022-10-31 19:58:15 -0400

    Se agrego el proyecto

---
+++ TarjetaApi/TarjetaApi/Controllers/TarjetasController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TarjetaApi.Models;
+
+namespace TarjetaApi.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class TarjetasController : ApiController
+    {
+        
+        public IHttpActionResult GetTarjeta()
+        {
+            using (TarjetaDBEntities db = new TarjetaDBEntities())
+            {
+                var t = db.Tarjeta.Select(x => new
+                {
+                    id = x.idTarjeta,
+                    x.nummeroTarjeta,
+                    x.fechaVencimiento,
+                    x.nombreTitular,
+                    x.cvv
+                }).ToList();                
+                return Ok(t);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostTarjeta(CrearTarjeta c)
+        {
+            using (TarjetaDBEntities db = new TarjetaDBEntities())
+            {
+                var t = new Tarjeta()
+                {
+                    nummeroTarjeta = c.nummeroTarjeta,
+                    fechaVencimiento = c.fechaVencimiento,
+                    nombreTitular = c.nombreTitular,
+                    cvv = c.cvv
+                };
+                var tarjetaRef = db.Tarjeta.Add(t);
+                db.SaveChanges();
+                return Ok(tarjetaRef);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult PutTarjeta(int? id, CrearTarjeta c)
+        {
+            using (TarjetaDBEntities db = new TarjetaDBEntities())
+            {
+                var t = new Tarjeta()
+                {
+                    nummeroTarjeta = c.nummeroTarjeta,
+                    fechaVencimiento = c.fechaVencimiento,
+                    nombreTitular = c.nombreTitular,
+                    cvv = c.cvv
+                };
+                var tarjetaRef = db.Tarjeta.Add(t);
+                db.SaveChanges();
+                return Ok(tarjetaRef);
+            }
+        }
+    }
+}
---
+++ TarjetaApi/TarjetaApi/Models/CrearTarjeta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TarjetaApi.Models
+{
+    public class CrearTarjeta
+    {
+        public string nummeroTarjeta { get; set; }
+        public string fechaVencimiento { get; set; }
+        public string nombreTitular { get; set; }
+        public string cvv { get; set; }
+    }
+}
